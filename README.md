bb3cd7390bc6efa740a0c61067c283383ebe60f2
Marcelo Lima <marcelocoder@gmail.com>
2020-08-07 09:51:47 -0400

    criando estado e consumindo api

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ O professor realiza o cadastro na plataforma informando os horários diponíveis
 Já o aluno pode escolher o professor e entrar em contato   
 
 ![Details web](screenshots/banner.png)
+
+
+## Configurações para criação do projeto
+- yarn add axios
--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {Link} from 'react-router-dom';
 import logoImg from '../../assets/images/logo.svg';
 import lnadingImg from '../../assets/images/landing.svg';
 import studyIcon from '../../assets/images/icons/study.svg';
 import giveClassesIcon from '../../assets/images/icons/give-classes.svg';
 import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg';
+import api from '../../services/api';
 import './styles.css';
 
 function Landing(){
+
+    const [totalConnections, setTotalConnections] = useState(0);
+    useEffect(() => {
+        api.get('connections').then(response =>{
+            const {total} = response.data;
+            setTotalConnections(total);
+        })
+    }, [totalConnections])
+
+
     return (
         <div id="page-landing">
             <div id="page-landing-content" className="container">
@@ -32,7 +43,7 @@ function Landing(){
                 </div>
 
                 <span className="total-conections">
-                    Total de 200 conexões já realizadas <img src={purpleHeartIcon} alt="coracao"/>
+                    Total de {totalConnections} conexões já realizadas <img src={purpleHeartIcon} alt="coracao"/>
                 </span>
             </div>
         </div>
